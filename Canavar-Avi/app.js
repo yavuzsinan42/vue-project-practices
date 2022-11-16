592dbff12a120c1caa61072000a7ef15f4670b86
Yavuz Sinan Aydemir <yavuzsinan.aydemir@koski.gov.tr>
2022-11-16 14:39:17 +0300

    Design and Methods Completed

---
+++ Canavar-Avi/app.js
@@ -0,0 +1,25 @@
+new Vue({
+    el:"#app",
+    data : {
+        player_heal : 100,
+        monster_heal : 100,
+        game_is_on : false,
+    },
+    methods:{
+       start_game : function(){
+            this.game_is_on = true;
+       },
+       attack : function(){
+
+       },
+       special_attack : function(){
+
+       },
+       heal_up: function(){
+
+       },
+       give_up(){
+
+       }
+    }
+})
